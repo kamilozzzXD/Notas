f4729915a398df6708f035c81e9c349c66b81173
kamilozzzXD <brayan.herrera.molano@unillanos.edu.co>
2024-06-13 13:50:14 -0500

    Primer commit de Notas

---
+++ src/main/java/com/api/AngularSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootApplication.class, args);
+	}
+}
