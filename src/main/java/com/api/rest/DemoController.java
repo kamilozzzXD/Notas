a8683c104b2fa5b420b34b615b69fe5a59f7688a
kamilozzzXD <brayan.herrera.molano@unillanos.edu.co>
2024-06-13 20:59:32 -0500

    Commit del login del backend

---
+++ src/main/java/com/api/rest/DemoController.java
@@ -0,0 +1,16 @@
+package com.api.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class DemoController {
+    @PostMapping(value = "demo")
+    public String welcome(){
+        return "welcome form secure endpoint";
+    }
+}
---
+++ src/main/java/com/api/security/securityConfig.java
@@ -0,0 +1,39 @@
+package com.api.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class securityConfig {
+
+    private final JwtAutheticationFilter jwtAutheticationFilter;
+    private final AuthenticationProvider authProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests(authRequest ->
+                    authRequest
+                        .antMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                        )
+                .sessionManagement(sessionManager->
+                        sessionManager
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAutheticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
